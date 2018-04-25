b10198941228a5d911d487049b6a1ca7d980d56a
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-04-25 17:46:15 +0200

    Fix linting errors

--- packages/core/src/components/ElementaControl.vue
+++ packages/core/src/components/ElementaControl.vue
@@ -20,6 +20,7 @@ export default {
       return `${this.control.type}Control`;
     },
     id() {
+      // eslint-disable-next-line no-underscore-dangle
       return `control-${this._uid}`;
     },
   },
